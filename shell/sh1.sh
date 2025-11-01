f6db5dbc543213399843f3a867a40ed035ea9204
GitEic-Bhavin <bhavsarbhavin16@gmail.com>
2025-11-01 23:47:25 +0530

    Add basic shell

---
+++ shell/sh1.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+SHELL="bash"
+
+echo "This is $SHELL shell."
+
+echo "script name is $0"
+
+echo "first argument is $1"
+
+echo "second argument is $2"
+
+echo "all argument is $@"
+
+echo "total argment is $#"
